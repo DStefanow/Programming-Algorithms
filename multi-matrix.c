a9ce9d033bc055edeccbd53a967da201246de8b6
Dobromir Stefanov <dobromirstefanov96@gmail.com>
2020-02-29 23:11:29 +0200

    Fix references in matrix array

--- multi-matrix.c
+++ multi-matrix.c
@@ -8,7 +8,7 @@ int **FIRST_MATRIX;
 
 // Declare used functions
 void get_user_info();
-void get_matrix_info(char count, int **matrix, size_t *rows, size_t *cols);
+void get_matrix_info(char count, int ***matrix, size_t *rows, size_t *cols);
 void destroy_matrix();
 void print_matrix(int **matrix, size_t rows, size_t cols);
 
@@ -16,9 +16,7 @@ void main()
 {
 	get_user_info();
 
-	printf("%d\n", FIRST_MATRIX[1][1]);
-
-	print_matrix(FIRST_MATRIX, FIRST_ROWS, FIRST_COLS);
+//	print_matrix(FIRST_MATRIX, FIRST_ROWS, FIRST_COLS);
 
 	destroy_matrix(FIRST_MATRIX);
 
@@ -30,10 +28,10 @@ void get_user_info()
 {
 	printf("------ Multi Matrix Program ------\n");
 
-	get_matrix_info(1, FIRST_MATRIX, &FIRST_ROWS, &FIRST_COLS);
+	get_matrix_info(1, &FIRST_MATRIX, &FIRST_ROWS, &FIRST_COLS);
 }
 
-void get_matrix_info(char count, int **matrix, size_t *rows, size_t *cols)
+void get_matrix_info(char count, int ***matrix, size_t *rows, size_t *cols)
 {
 	// Get rows and columns
 	printf("Enter %d-st matrix rows count: ", count);
@@ -42,15 +40,15 @@ void get_matrix_info(char count, int **matrix, size_t *rows, size_t *cols)
 	scanf("%zu", &(*cols));
 
 	// Alocate memory for matrix
-	matrix = (int**)malloc(sizeof(int) * (*rows));
+	*matrix = (int**)malloc(sizeof(int) * (*rows));
 	for (size_t i = 0; i < (*rows); i++) {
-		matrix[i] = (int*)malloc(sizeof(int) * (*cols));
+		(*matrix)[i] = (int*)malloc(sizeof(int) * (*cols));
 	}
 
 	for(size_t i = 0; i < *rows; i++) {
 		for(size_t j = 0; j < *cols; j++) {
 			printf("Enter (%zu,%zu) for %d matrix: ", i, j, count);
-			matrix[i][j] = j;
+			*matrix[i][j] = j;
 
 			printf("%d", *matrix[i][j]);
 //			scanf("%d", &matrix[i][j]);
